79888b5d3c3be26094c5b699d3fbb93bc9faca37
Masahiro Tanaka <masahiro@asial.co.jp>
2016-01-22 19:07:06 +0900

    Fix.

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-FILE=`find build -name *.apk`
-curl -F "status=2" -F "notify=1" -F "ipa=@{$FILE}" -H "X-HockeyAppToken: $HOCKEY_APP_TOKEN" https://rink.hockeyapp.net/api/2/apps/upload
+
+FILE=`find build -name *.apk|head -n 1`
+curl -F "status=2" -F "notify=1" -F "ipa=@$FILE" -H "X-HockeyAppToken: $HOCKEY_APP_TOKEN" https://rink.hockeyapp.net/api/2/apps/upload
