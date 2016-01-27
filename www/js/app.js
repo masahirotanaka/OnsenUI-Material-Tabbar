28df08a59c7daffbfd055d62970dab993b1b4e32
Masahiro Tanaka <masahiro@asial.co.jp>
2016-01-27 09:01:07 +0900

    Using Cordova API to get contacts.

--- www/js/app.js
+++ www/js/app.js
@@ -1,14 +1,43 @@
+var contacts = [];
+
 ons.ready(function() {
-	document.querySelector('#myNavigator').addEventListener('init', function(event) {
+	document.addEventListener('init', function(event) {
 		page = event.target;
 
 		if (page.id === 'all_contacts') {
-			ons.myApp.contactListController(page, ons.myApp.createAllListItem, ons.myApp.myData.slice(0).sort());
+			ons.myApp.contactListController(page, ons.myApp.createAllListItem, contacts);
 		} else if (page.id === 'fav_contacts') {
-			ons.myApp.contactListController(page, ons.myApp.createFavListItem, ons.myApp.myData.slice(0, 9).sort());
+			ons.myApp.contactListController(page, ons.myApp.createAllListItem, contacts.slice(0, 9));
 		} else if (page.id === 'details_contacts') {
 			ons.myApp.contactDetailsController(page);
 		}
 	});
 });
 
+document.addEventListener("deviceready", function() {
+  var fields = [navigator.contacts.fieldType.displayName, navigator.contacts.fieldType.name];
+  
+  navigator.contacts.find(fields, function(_contacts) {
+    for (var i = 0; i < _contacts.length; i++) {
+      if (_contacts[i].displayName != null && _contacts[i].phoneNumbers != null && _contacts[i].emails != null) {
+        contacts.push(_contacts[i]);
+      }
+    }
+    
+    contacts.sort(function(a, b) {
+        if (a.displayName < b.displayName)
+          return -1;
+        else if (a.displayName > b.displayName)
+          return 1;
+        else 
+          return 0;
+    });
+    
+    ons.myApp.contactListController(page, ons.myApp.createAllListItem, contacts);
+    document.querySelector("ons-progress-bar").style.display = "none";
+    
+  }, function(e) {
+    alert("Error");
+  });
+  
+})
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,14 +1,65 @@
+ons.myApp = {};
+
 ons.myApp.contactListController = function (page, createListItem, myContacts) {
 	var list = page.querySelector('ons-list'),
 		div = document.createElement('div');
-	div.innerHTML = createListItem(myContacts[0], 0, myContacts[0].charAt(0));
+    
+  if (myContacts.length == 0) return;
+  
+	div.innerHTML = createListItem(myContacts[0].displayName, 0, myContacts[0].displayName.charAt(0));
 	list.appendChild(div.firstChild);
 	for (var i = 1; i < myContacts.length; i++) {
-		div.innerHTML = createListItem(myContacts[i], i, (myContacts[i].charAt(0) !== myContacts[i - 1].charAt(0)) ? myContacts[i].charAt(0) : '');
+		div.innerHTML = createListItem(myContacts[i].displayName, i, (myContacts[i].displayName.charAt(0) !== myContacts[i - 1].displayName.charAt(0)) ? myContacts[i].displayName.charAt(0) : '');
 		list.appendChild(div.firstChild);
 	}
 }
 
-ons.myApp.contactDetailsController = function (page) {
-	page.querySelector('ons-toolbar .center').innerHTML = document.querySelector('#myNavigator').getCurrentPage().options.person;
+ons.myApp.contactDetailsController = function (pageDom) {
+  var page = document.querySelector('#myNavigator').getCurrentPage();
+  var contact = contacts[page.options.index];
+	pageDom.querySelector('ons-toolbar .center').innerHTML = contact.displayName;
+	pageDom.querySelector('span.phone-number').innerHTML = contact.phoneNumbers[0].value;
+	pageDom.querySelector('span.phone-type').innerHTML = contact.phoneNumbers[0].type;
+	pageDom.querySelector('span.email-address').innerHTML = contact.emails[0].value;
+	pageDom.querySelector('span.email-type').innerHTML = contact.emails[0].type;
+  
+  pageDom.querySelector('.do-phone-call').addEventListener("click", function() {
+    document.location.href = "tel:" + contact.phoneNumbers[0].value;
+  });
+  pageDom.querySelector('.do-send-email').addEventListener("click", function() {
+    document.location.href = "mailto:" + contact.emails[0].value;
+  });
 }
+
+ons.myApp.showDetail = function(index) {
+  document.querySelector('#myNavigator').pushPage('html/details_contact.html', {animation: 'lift', index: index});
+}
+
+ons.myApp.createAllListItem = function(name, index, initials) {
+
+    return '<ons-list-item modifier="material" onclick="ons.myApp.showDetail(' + index + ')">' +
+          '<div class="list__item__left">' +
+             '<span class="contact-initials">' + initials + '</span>' +
+          '</div>' +
+          '<div class="list__item__center">' +
+            '<ons-ripple color="rgba(0, 0, 0, 0.1)"></ons-ripple>' +
+            '<img src="images/profile-image-0' + (index % 9 + 1) + '.png" class="list__item__thumbnail">' +
+            '<div>' + name + '</div>' +
+          '</div>' +
+        '</ons-list-item>'
+    ;
+};
+
+ons.myApp.createFavListItem = function(name, index) {
+
+    return '<ons-list-item modifier="material" onclick="document.querySelector(\'#myNavigator\').pushPage(\'html/details_contact.html\', {animation: \'lift\', person: \'' + name + '\'})">' +
+          '<ons-ripple color="rgba(0, 0, 0, 0.1)"></ons-ripple>' +
+          '<div class="list__item__left">' +
+            '<img src="images/profile-image-0' + (index % 9 + 1) + '.png" class="list__item__thumbnail">' +
+          '</div>' +
+          '<div class="list__item__center">' +
+            name +
+          '</div>' +
+        '</ons-list-item>'
+    ;
+};
